2576d10bbc0950fc978d1f971bd9712e82d7985a
piesuke <piesuke0727@gmail.com>
2018-10-09 15:59:43 +0900

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+  # def gravater_for(user)
+  #  # gravater_id = Digest::MDS::hexdigest(user.email.downcase)
+  #  gravater_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+  #  image_tag(gravater_url, alt:user.name, class: "gravater")
+  # end
 end
